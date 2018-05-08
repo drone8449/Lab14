d6253a5a788e5653bf463dda483c92e9333d6674
leny <leny@172.17.128.95>
2018-05-09 04:19:42 +0900

    Add_prob5

--- src/main/java/edu/handong/csee/java/lab14/prob5/My_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob5/My_main.java
@@ -1,5 +1,22 @@
 package edu.handong.csee.java.lab14.prob5;
+import java.util.Scanner;
 
 public class My_main {
-
+	public static void main(String[] args) {
+		try {
+			Scanner keyboard = new Scanner(System.in);
+			String str = keyboard.nextLine();
+			My_main.myTest(str);
+		} catch(MyException e) {
+			System.out.println("Inside catch block: " + e);
+		}
+	}
+	
+	static void myTest(String str) throws MyException {
+		if(str.equals("null")) {
+			throw new MyException("String val is null");
+		}
+		else
+			System.out.println("String val is: " + str);
+	}
 }
