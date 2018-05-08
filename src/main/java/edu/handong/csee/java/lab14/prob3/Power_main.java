30e0762c96d8400985bd4c2ba3e194bb9f94aa18
leny <leny@172.17.128.95>
2018-05-08 22:23:58 +0900

    Add_prob3

--- src/main/java/edu/handong/csee/java/lab14/prob3/Power_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Power_main.java
@@ -1,5 +1,20 @@
 package edu.handong.csee.java.lab14.prob3;
+import java.util.Scanner;
 
 public class Power_main {
-
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		Powercalc cal = new Powercalc();
+		
+		while(keyboard.hasNextInt()) {
+			int n = keyboard.nextInt();
+			int p = keyboard.nextInt();
+			
+			try {
+				System.out.println(cal.power(n, p));
+			} catch (Exception e){
+				System.out.println(e);
+			}
+		}
+	}
 }
