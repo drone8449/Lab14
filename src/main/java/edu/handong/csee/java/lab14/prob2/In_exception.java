85d96d1669d69e1341ffb77a216d040e4886ded4
leny <leny@172.17.128.157>
2018-05-08 21:22:56 +0900

    Add_prob2

--- src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
@@ -1,5 +1,29 @@
 package edu.handong.csee.java.lab14.prob2;
+import java.util.*;
 
 public class In_exception {
-
+	private int x = 0, y = 0;
+	
+	public In_exception(){
+	}
+	
+	public void detectError(){
+		try {
+			Scanner keyboard = new Scanner(System.in);
+			System.out.print("x: ");
+			x = keyboard.nextInt();
+			System.out.print("y: ");
+		    y = keyboard.nextInt();
+		    System.out.print(this.x/this.y);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("java.Lang.ArithmeticException: " + e.getMessage());
+		}
+		catch(InputMismatchException e) {
+			System.out.println("java.util.InputMismatchException");
+		}
+		catch(Exception e) {
+			System.out.println("Some other exception has occurred: " + e.getMessage());
+		}
+	}
 }
--- src/main/java/edu/handong/csee/java/lab14/prob2/Inexcep_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/Inexcep_main.java
@@ -1,5 +1,8 @@
 package edu.handong.csee.java.lab14.prob2;
 
 public class Inexcep_main {
-
+	public static void main(String[] args) {
+		In_exception obj = new In_exception();
+		obj.detectError();
+	}
 }
