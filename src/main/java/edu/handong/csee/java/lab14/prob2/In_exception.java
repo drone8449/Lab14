e621704fabd47b0a4ece55a7b43e079bcb62bd92
leny <leny@172.17.128.95>
2018-05-10 01:46:13 +0900

    Add_Comment_prob2

--- src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
@@ -1,29 +1,29 @@
-package edu.handong.csee.java.lab14.prob2;
-import java.util.*;
+package edu.handong.csee.java.lab14.prob2;  // package name
+import java.util.*;   // import util.* class
 
-public class In_exception {
-	private int x = 0, y = 0;
+public class In_exception {   // class name
+	private int x = 0, y = 0;   // declare integer x, y
 	
-	public In_exception(){
+	public In_exception(){   // constructor
 	}
 	
-	public void detectError(){
-		try {
-			Scanner keyboard = new Scanner(System.in);
-			System.out.print("x: ");
-			x = keyboard.nextInt();
-			System.out.print("y: ");
-		    y = keyboard.nextInt();
-		    System.out.print(this.x/this.y);
+	public void detectError(){   // method name
+		try {   // try code
+			Scanner keyboard = new Scanner(System.in);   // declare object 'keyboard' to use Scanner Class
+			System.out.print("x: ");   // print message
+			x = keyboard.nextInt();   // next input is x
+			System.out.print("y: ");   // print message
+		    y = keyboard.nextInt();   // next input is y
+		    System.out.print(this.x/this.y);   // print x/y
 		}
-		catch(ArithmeticException e) {
-			System.out.println("java.Lang.ArithmeticException: " + e.getMessage());
+		catch(ArithmeticException e) {   // catch ArithmeticException
+			System.out.println("java.Lang.ArithmeticException: " + e.getMessage());   // print error message
 		}
-		catch(InputMismatchException e) {
-			System.out.println("java.util.InputMismatchException");
+		catch(InputMismatchException e) {   // catch InputMismatchException
+			System.out.println("java.util.InputMismatchException");   // print error message
 		}
-		catch(Exception e) {
-			System.out.println("Some other exception has occurred: " + e.getMessage());
+		catch(Exception e) {   // catch Exception
+			System.out.println("Some other exception has occurred: " + e.getMessage());   // print error message
 		}
 	}
 }
--- src/main/java/edu/handong/csee/java/lab14/prob2/Inexcep_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/Inexcep_main.java
@@ -1,8 +1,8 @@
-package edu.handong.csee.java.lab14.prob2;
+package edu.handong.csee.java.lab14.prob2;   // package name
 
-public class Inexcep_main {
-	public static void main(String[] args) {
-		In_exception obj = new In_exception();
-		obj.detectError();
+public class Inexcep_main {   // class name
+	public static void main(String[] args) {   // main method
+		In_exception obj = new In_exception();   // declare object 'obj' to use In_exception Class
+		obj.detectError();   // run detectError method
 	}
 }
