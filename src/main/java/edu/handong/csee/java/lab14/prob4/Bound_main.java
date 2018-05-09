869e8ab61e07b71d98a81e9bb1f015011fa92e88
leny <leny@172.17.128.95>
2018-05-10 02:23:31 +0900

    Add_Comment_prob4

--- src/main/java/edu/handong/csee/java/lab14/prob4/Bound_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Bound_main.java
@@ -1,21 +1,21 @@
-package edu.handong.csee.java.lab14.prob4;
-import java.util.Scanner;
+package edu.handong.csee.java.lab14.prob4;   // package name
+import java.util.Scanner;   // import Scanner class
 
-public class Bound_main {
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		int num = 0;
-		Boundary arr = new Boundary();
-		boolean repeat = true;
-		while(repeat) {
-			try {
-				System.out.print("Enter an integer: ");
-				num = keyboard.nextInt();
-				arr.receive(num);
+public class Bound_main {   // class name
+	public static void main(String[] args) {   // main method
+		Scanner keyboard = new Scanner(System.in);   // declare object 'keyboard' to use Scanner Class
+		int num = 0;   // declare num
+		Boundary arr = new Boundary();   // declare object 'arr' to use Boundary Class
+		boolean repeat = true;   // declare boolean repeat
+		while(repeat) {   // repeat until repeat is false
+			try {   // try code
+				System.out.print("Enter an integer: ");   // print message
+				num = keyboard.nextInt();   // input is num
+				arr.receive(num);   // run receive method
 			}
-			catch(ArrayIndexOutOfBoundsException e) {
-				System.out.println("Invalid array index access!");
-				repeat = false;
+			catch(ArrayIndexOutOfBoundsException e) {   // catch ArrayIndexOutOfBoundsException
+				System.out.println("Invalid array index access!");   // print message
+				repeat = false;   // repeat is false 
 			}
 		}
 	}
--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -1,12 +1,13 @@
-package edu.handong.csee.java.lab14.prob4;
+package edu.handong.csee.java.lab14.prob4;   // package name
 
-public class Boundary {
-	private static int [] arr = new int[5];
-	private static int cnt = 0;
-	public void receive(int num) throws ArrayIndexOutOfBoundsException {
-		int i = cnt;
-		arr[i] = num;
-		cnt++;
-		System.out.printf("arr[%d] <- %d\n", i, num);
+public class Boundary {   // class name
+	private static int [] arr = new int[5];   // declare 'arr' to use 5-size integer array 
+	private static int cnt = 0;   // declare integer
+	
+	public void receive(int num) throws ArrayIndexOutOfBoundsException {   // method having parameter num and throwing ArrayIndexOutOfBoundsException
+		int i = cnt;   // cnt is i
+		arr[i] = num;   // num is arr[i]
+		cnt++;  // cnt increasing by 1
+		System.out.printf("arr[%d] <- %d\n", i, num);   // print message
 	}
 }
