aca3e7a6d6146cf0e72f53c0674f1577a59fd5a3
leny <leny@172.17.128.95>
2018-05-09 18:55:08 +0900

    Add_prob6

--- src/main/java/edu/handong/csee/java/lab14/prob6/Custom_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/Custom_main.java
@@ -1,5 +1,37 @@
 package edu.handong.csee.java.lab14.prob6;
+import java.util.Scanner;
 
 public class Custom_main {
+	public static void main(String[] args) {
+		String name, race;
+		int age = 0;
+		Scanner keyboard = new Scanner(System.in);
+		CustomID obj = new CustomID();
+		boolean repeat = true;
 
+	while(repeat) {
+	   try {
+		   if(obj.getState() == 0) {
+			   System.out.print("Enter your name: ");
+			   name = keyboard.nextLine();
+			   obj.set_name(name);
+	       }
+	       else if(obj.getState() == 1) {
+		       System.out.print("Enter your age: ");
+		       age = keyboard.nextInt();
+		       keyboard.nextLine();
+		       obj.set_age(age);
+	        }
+	       else if(obj.getState() == 2) {
+	    	       System.out.print("Enter your race: ");
+	    	       race = keyboard.nextLine();
+	    	       obj.set_race(race);
+	    	   }
+         }
+	   catch(Exception e){
+		   System.out.println(e.getMessage());
+		   continue;
+		 }
+	  }
+   }
 }
