9a45d25451c5f0cd532d83af1246f2d053b0d650
leny <leny@172.17.128.95>
2018-05-10 02:01:38 +0900

    Add_Comment_prob3

--- src/main/java/edu/handong/csee/java/lab14/prob3/Power_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Power_main.java
@@ -1,19 +1,19 @@
-package edu.handong.csee.java.lab14.prob3;
-import java.util.Scanner;
+package edu.handong.csee.java.lab14.prob3;   // package name
+import java.util.Scanner;   // import Scanner class
 
-public class Power_main {
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		Powercalc cal = new Powercalc();
+public class Power_main {   // class name
+	public static void main(String[] args) {   // main method
+		Scanner keyboard = new Scanner(System.in);   // declare object 'keyboard' to use Scanner Class
+		Powercalc cal = new Powercalc();   // declare object 'cal' to use Powercalc Class
 		
-		while(keyboard.hasNextInt()) {
-			int n = keyboard.nextInt();
-			int p = keyboard.nextInt();
+		while(keyboard.hasNextInt()) {   // repeat until keyboard has not next value
+			int n = keyboard.nextInt();   // input is n
+			int p = keyboard.nextInt();   // input is p
 			
-			try {
-				System.out.println(cal.power(n, p));
-			} catch (Exception e){
-				System.out.println(e);
+			try {   // try code
+				System.out.println(cal.power(n, p));   // print power value
+			} catch (Exception e){   // catch Exception
+				System.out.println(e);   // print error message 
 			}
 		}
 	}
--- src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
@@ -1,16 +1,16 @@
-package edu.handong.csee.java.lab14.prob3;
+package edu.handong.csee.java.lab14.prob3;   // package name
 
-public class Powercalc {
-	public long power(int n, int p) throws Exception {
-		long result = 0;
+public class Powercalc {   // class name
+	public long power(int n, int p) throws Exception {   // method having n, p parameter and throwing Exception
+		long result = 0;    // declare long result
 		
-		result = (long)Math.pow(n, p);
-		if(n < 0 || p < 0) {
-			throw new Exception("n or p should not be negative.");
+		result = (long)Math.pow(n, p);   // result is n^p
+		if(n < 0 || p < 0) {   // if n is lower than 0 or p is lower than 0
+			throw new Exception("n or p should not be negative.");   // set error code
 		}
-		if(n == 0 && p == 0) {
-			throw new Exception("n and p should not be zero.");
+		if(n == 0 && p == 0) {   // if n is 0 and p is 0
+			throw new Exception("n and p should not be zero.");   // set error code
 		}
-		return result;
+		return result;   // return result value
 	}
 }
