2b3c630ec6889f4ca18f3b99e35e6d545c0dd0be
leny <leny@172.17.128.95>
2018-05-10 20:58:14 +0900

    Add_Comment_prob6

--- src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
@@ -1,50 +1,50 @@
-package edu.handong.csee.java.lab14.prob6;
+package edu.handong.csee.java.lab14.prob6;   // package name
 
-public class CustomID extends Exception {
-	private String[] races = new String[]{"Vulcans", "Romulan", "Klingons"};
-	private String name = "";
-	private int age = 0;
-	private String race = "";
-	private int state = 0;
+public class CustomID extends Exception {   // inheritance from Exception
+	private String[] races = new String[]{"Vulcans", "Romulan", "Klingons"};   // declare string array having race value
+	private String name = "";   // declare string having name value 
+	private int age = 0;   // declare integer having age value
+	private String race = "";   // declare string having race value
+	private int state = 0;   // declare integer
 
-	public void set_name(String str) throws Exception {
-	   if(str.length() < 5) {
-		   state = 0;
-		   throw new Exception("Your name is short! Try again!");
-	} else {
-		this.name = str;
-		System.out.println("Name is valid");
-		System.out.println("Name: " + this.name);
-		state++;
+	public void set_name(String str) throws Exception {   // method having parameter str and throwing Exception
+	   if(str.length() < 5) {   // name's length is lower than 5
+		   state = 0;   // initialize state value
+		   throw new Exception("Your name is short! Try again!");   // set error message
+	} else {   // else
+		this.name = str;   // name is str
+		System.out.println("Name is valid");   // print message
+		System.out.println("Name: " + this.name);   // print name
+		state++;   // state increase by 1
 	}
   }
-    public void set_age(int num) throws Exception {
-	   if(num < 18) {
-		   state = 1;
-		   throw new Exception("You are too young! Try again!");
-	} else {
-		this.age = num;
-		System.out.println("Age is valid");
-		System.out.println("Age: " + this.age);
-		state++;
+    public void set_age(int num) throws Exception {   // method having parameter num and throwing Exception
+	   if(num < 18) {   // if age is lower than 18
+		   state = 1;   // state is 1
+		   throw new Exception("You are too young! Try again!");   // set error message
+	} else {   // else
+		this.age = num;  // num is age
+		System.out.println("Age is valid");   // print message
+		System.out.println("Age: " + this.age);   // print age
+		state++;   // state increase by 1
 	}
 }
 
-    public void set_race(String race) throws Exception{
-	   for(int i = 0; i < races.length; i++) {
-		   if(races[i].equals(race)) {
-			  this.race = races[i];
-			  System.out.println("Race is valid");
-			  System.out.println("Race: " + races[i]);
-			  state = 0;
-			  return;
+    public void set_race(String race) throws Exception{   // method having parameter race and throwing Exception
+	   for(int i = 0; i < races.length; i++) {   // repeat races's length
+		   if(races[i].equals(race)) {   // if race is same with one of races string array values 
+			  this.race = races[i];   // set race value
+			  System.out.println("Race is valid");   // print message
+			  System.out.println("Race: " + races[i]);   // print race
+			  state = 0;   // initialize state value
+			  return;   // return
             }
         }
-        state = 2;
-        throw new Exception("Human! Try again.");
+        state = 2;   // state is 2
+        throw new Exception("Human! Try again.");   // set error message
 }
 
-    public int getState() {
-       return state;
+    public int getState() {   // method name
+       return state;  // return state
     }
 }
--- src/main/java/edu/handong/csee/java/lab14/prob6/Custom_main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/Custom_main.java
@@ -1,36 +1,36 @@
-package edu.handong.csee.java.lab14.prob6;
-import java.util.Scanner;
+package edu.handong.csee.java.lab14.prob6;   // package name
+import java.util.Scanner;   // import Scanner class
 
-public class Custom_main {
-	public static void main(String[] args) {
-		String name, race;
-		int age = 0;
-		Scanner keyboard = new Scanner(System.in);
-		CustomID obj = new CustomID();
-		boolean repeat = true;
+public class Custom_main {   // class name 
+	public static void main(String[] args) {   // main method
+		String name, race;   // declare strings
+		int age = 0;   // declare integer
+		Scanner keyboard = new Scanner(System.in);   // declare object 'keyboard' to use Scanner Class
+		CustomID obj = new CustomID();   // declare object 'obj' to use CustomID Class
+		boolean repeat = true;   // declare boolean 
 
-	while(repeat) {
-	   try {
-		   if(obj.getState() == 0) {
-			   System.out.print("Enter your name: ");
-			   name = keyboard.nextLine();
-			   obj.set_name(name);
+	while(repeat) {    // repeat until repeat is false
+	   try {   // try code
+		   if(obj.getState() == 0) {   // if state is 0
+			   System.out.print("Enter your name: ");   // print message
+			   name = keyboard.nextLine();   // input is name
+			   obj.set_name(name);   // set name value
 	       }
-	       else if(obj.getState() == 1) {
-		       System.out.print("Enter your age: ");
-		       age = keyboard.nextInt();
-		       keyboard.nextLine();
-		       obj.set_age(age);
+	       else if(obj.getState() == 1) {   // if state is 1
+		       System.out.print("Enter your age: ");   // print message
+		       age = keyboard.nextInt();   // input is age
+		       keyboard.nextLine();   // get enter value 
+		       obj.set_age(age);   // set age value
 	        }
-	       else if(obj.getState() == 2) {
-	    	       System.out.print("Enter your race: ");
-	    	       race = keyboard.nextLine();
-	    	       obj.set_race(race);
+	       else if(obj.getState() == 2) {   // if state is 2
+	    	       System.out.print("Enter your race: ");   // print message
+	    	       race = keyboard.nextLine();   // input is race
+	    	       obj.set_race(race);   // set race value
 	    	   }
          }
-	   catch(Exception e){
-		   System.out.println(e.getMessage());
-		   continue;
+	   catch(Exception e){   // catch exception
+		   System.out.println(e.getMessage());   // print error message
+		   continue;   // continue
 		 }
 	  }
    }
